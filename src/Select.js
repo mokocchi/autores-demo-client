ae5fd3fb0ddc9e8665149044e8611b24cdb1532c
Jose Arcidiacono <josefine2_94@hotmail.com>
2019-12-29 17:28:18 -0300

    Modificar Select

--- src/Select.js
+++ src/Select.js
@@ -4,11 +4,11 @@ import { Form } from 'react-bootstrap'
 function Select(props) {
     return (
         <Form.Group controlId={props.controlId}>
-            { props.label && <Form.Label>{props.label}</Form.Label>}
-            <Form.Control defaultValue={props.defaultValue} as="select" name={props.name} onChange={props.onChange}>
-            <option value="" disabled>{props.placeholder}</option>
+            {props.label && <Form.Label>{props.label}</Form.Label>}
+            <Form.Control defaultValue={props.defaultValue} as="select" name={props.name} onChange={props.onChange} disabled={props.disabled}>
+                {props.placeholder && <option value="" disabled>{props.placeholder}</option>}
                 {props.options.map((item, index) =>
-                    <option value={props.value? item[props.value]: item} key={index}>{props.field ? item[props.field] : item}</option>
+                    <option value={props.value ? item[props.value] : item} key={index}>{props.field ? item[props.field] : item}</option>
                 )}
             </Form.Control>
         </Form.Group>
