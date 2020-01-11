3daf69ca9c771f6339ac575b3a81c7d0631f0395
Jose Arcidiacono <josefine2_94@hotmail.com>
2020-01-11 01:14:46 -0300

    Arreglar menu, borrar /docs

--- src/Menu.js
+++ src/Menu.js
@@ -8,9 +8,9 @@ function Menu() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/#/actividades/">Lista Actividades</Nav.Link>
-                    <Nav.Link href="/#/nuevaActividad/">Crear Actividad</Nav.Link>
+                    <Nav.Link href="/autores-demo-client/">Home</Nav.Link>
+                    <Nav.Link href="/autores-demo-client/#/actividades/">Lista Actividades</Nav.Link>
+                    <Nav.Link href="/autores-demo-client/#/nuevaActividad/">Crear Actividad</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
