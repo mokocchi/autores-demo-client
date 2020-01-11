4209ebac7363bd42529e014c3278b6b7dbe36dff
Jose Arcidiacono <josefine2_94@hotmail.com>
2020-01-11 00:19:16 -0300

    build a docs

---
+++ docs/precache-manifest.b03759500b7e3df1fc3ef3eb0c72bd10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2148d115df0d639b3bf8c91814c4250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3daa9c40570e1ec318",
+    "url": "/static/js/2.8634afeb.chunk.js"
+  },
+  {
+    "revision": "c9a4567bc6a58c0a7f833a03df487fa2",
+    "url": "/static/js/2.8634afeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04bbac6bb0266970c9f5",
+    "url": "/static/js/main.23678939.chunk.js"
+  },
+  {
+    "revision": "4ce33c38acf63e59c0c1",
+    "url": "/static/js/runtime-main.2c87c1dc.js"
+  }
+]);
