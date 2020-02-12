5cad656b62cdffc87ab2fba762b583345adffc5d
Jose Arcidiacono <josefine2_94@hotmail.com>
2020-02-12 10:41:40 -0300

    Agregar login redirect para 403

--- src/Unauthorized.js
+++ src/Unauthorized.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import loggedIn from './loggedIn';
 
-export default function Unauthorized() {
+export default loggedIn(() => {
     return <div><h2>No tenés permiso para ver esta página</h2></div>
-}
+})
