ba73e56988930010e3fe9e0290c13ecd9111b3cb
Jose Arcidiacono <josefine2_94@hotmail.com>
2020-05-14 12:21:52 -0300

    Agregar chequeo depositos antes que recolección

--- src/components/Actividad/AddTarea/TareasActionList.js
+++ src/components/Actividad/AddTarea/TareasActionList.js
@@ -8,9 +8,10 @@ const AddTareasActionList = (props) => {
     return (
         <div>
             <h3>Tareas de la actividad {props.clone && `(${props.remainingTareas.length} más)`}</h3>
-            {!props.clone && <Alert variant="info"><Icon name="md-information-circle-outline" font="Ionicons" color="teal" size={"1rem"} /> Hacé click y arrastrá una tarea hacia arriba o hacia abajo para reordenarla</Alert>}
+            <Alert variant="info"><Icon name="md-information-circle-outline" font="Ionicons" color="teal" size={"1rem"} /> Hacé click y arrastrá una tarea hacia arriba o hacia abajo para reordenarla</Alert>
+            {props.error && <Alert variant="danger"><Icon name="md-alert" font="Ionicons" color="burgundy" size={"1rem"} />  {props.error}</Alert>}
             <ActionList variant="success" items={props.chosenTareas} action={true} onlyLastAction={props.clone} onClick={props.onClick} field={"nombre"} value={"id"}
-            onReorder={props.onReorder} reorder={!props.clone} group={"addTareas"} /> 
+                onReorder={props.onReorder} reorder={true} group={"addTareas"} />
         </div>
     )
 }
--- src/components/Actividad/AddTarea/TareasActionListContainer.js
+++ src/components/Actividad/AddTarea/TareasActionListContainer.js
@@ -8,6 +8,9 @@ import { unchooseTarea, setChosenTareas } from '../../../redux/actions';
 import { TIPO_COLLECT, TIPO_SELECT, TIPO_MULTIPLE, TIPO_CAMERA_INPUT, TIPO_TEXT_INPUT, TIPO_NUMBER_INPUT, TIPO_COUNTERS, TIPO_AUDIO_INPUT, TIPO_DEPOSIT, TIPO_SIMPLE, TIPO_GPS_INPUT } from '../../../config';
 
 class AddTareasActionListContainer extends Component {
+    state = {
+        error: null
+    }
 
     onClick = (item) => {
         this.props.dispatch(unchooseTarea(item.id));
@@ -16,6 +19,41 @@ class AddTareasActionListContainer extends Component {
     onReorder = (event, previousIndex, nextIndex) => {
         const newList = reorder(this.props.chosenTareas, previousIndex, nextIndex);
         this.props.dispatch(setChosenTareas(newList));
+        this.checkOrder();
+    }
+
+    componentDidMount() {
+        this.checkOrder();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.chosenTareas.length !== prevProps.chosenTareas.length) {
+            this.checkOrder();
+        }
+    }
+
+    checkOrder = () => {
+        let recoleccion = false;
+        let depositoOK = true;
+        for (let index = 0; index < this.props.chosenTareas.length; index++) {
+            const tarea = this.props.chosenTareas[index];
+            if ((tarea.tipo.codigo === TIPO_DEPOSIT) && !recoleccion) {
+                depositoOK = false;
+                break;
+            }
+            if (tarea.tipo.codigo === TIPO_COLLECT) {
+                recoleccion = true;
+            }
+        }
+        if (!depositoOK) {
+            this.setState({
+                error: "En el orden de tareas, hay depósitos antes de tareas de recolección. Reordená para arreglarlo."
+            })
+        } else {
+            this.setState({
+                error: null
+            })
+        }
     }
 
     getIconName(tipo) {
@@ -50,6 +88,7 @@ class AddTareasActionListContainer extends Component {
     render() {
         const clonedTareas = this.props.clonedTareas;
         let tareas = [];
+
         if (this.props.clone) {
             tareas = this.props.chosenTareas.map(
                 (tarea, index) => {
@@ -75,7 +114,7 @@ class AddTareasActionListContainer extends Component {
             );
         }
         return <AddTareasActionList clone={this.props.clone} remainingTareas={this.props.remainingTareas} chosenTareas={tareas} onClick={this.onClick}
-            onReorder={this.onReorder} />
+            onReorder={this.onReorder} error={this.state.error} />
     }
 }
 
